ea094acb51596b317cbc953bcb98ff164eda3f1f
KV.Dhanunjaya Redddy <KV.DhanunjayaReddy@godigit.com>
2024-07-31 23:20:06 +0530

    Binary numbers

---
+++ Queue/BinaryNumbers.java
@@ -0,0 +1,24 @@
+package Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryNumbers {
+    public static void generateBinaryNumbers(int n) {
+        Queue<String> queue = new LinkedList<>();
+        queue.add("1");
+
+        for (int i = 1; i <= n; i++) {
+            String current = queue.poll();
+            System.out.println(current);
+
+            queue.add(current + "0");
+            queue.add(current + "1");
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 10; // Change this value to generate binary numbers up to n
+        generateBinaryNumbers(n);
+    }
+}
